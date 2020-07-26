0ec1f44478ac835c1fd19b90d1a6a8ef3aa9d3d0
abhiandthetruth <abhiandthetruth@gmail.com>
2020-07-26 16:56:43 +0530

    Fix query try 18

--- geekhaven/addmember.php
+++ geekhaven/addmember.php
@@ -116,7 +116,7 @@
                             $result = pg_query($connection,$query);
                             while($row = pg_fetch_assoc($result)){
                                 $member_id =$row['member_id'];
-                                $query = "SELECT * FROM member WHERE `member_id`='$member_id'";
+                                $query = "SELECT * FROM member WHERE member_id='$member_id'";
                                 $query_run = pg_query($connection,$query);
                                 $res = pg_fetch_assoc($query_run);
                                 $name =$res['name'];
