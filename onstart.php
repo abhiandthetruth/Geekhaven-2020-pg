d1281c2321a912e416d955446e8d6737a2bf354b
abhiandthetruth <abhiandthetruth@gmail.com>
2020-07-26 14:57:23 +0530

    Fix query try 1

--- onstart.php
+++ onstart.php
@@ -1,34 +1,34 @@
 <?php
     $connection = pg_connect(getenv("DATABASE_URL"));
     $query = "CREATE TABLE social_handles (
-        `social_handles_id` varchar(255),
-        `github` varchar(255),
-        `mail` varchar(255),
-        `facebook`varchar(255),
-        `instagram` varchar(255),
-        `codechef` varchar(255),
-        `codeforces` varchar(255),
-        `linkedin` varchar(255),
-        `hackerrank` varchar(255),
-        `hackerearth` varchar(255),
-        `twitter` varchar(255),
+        social_handles_id varchar(255),
+        github varchar(255),
+        mail varchar(255),
+        facebook varchar(255),
+        instagram varchar(255),
+        codechef varchar(255),
+        codeforces varchar(255),
+        linkedin varchar(255),
+        hackerrank varchar(255),
+        hackerearth varchar(255),
+        twitter varchar(255),
         PRIMARY KEY (social_handles_id)       
     );";
 
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE member (
-        `name` varchar(255),
-        `roll_no` varchar(255),
-        `image` longblob,
-        `description`varchar(12000),
-        `member_id` varchar(255),
-        `hof` varchar(255),
-        `social_handles` varchar(255),
-        `cred_id` varchar(255),
-        `post` varchar(255),
-        `wing` varchar(255),
-        `session` varchar(255),
+        name varchar(255),
+        roll_no varchar(255),
+        image longblob,
+        description varchar(12000),
+        member_id varchar(255),
+        hof varchar(255),
+        social_handles varchar(255),
+        cred_id varchar(255),
+        post varchar(255),
+        wing varchar(255),
+        session varchar(255),
         PRIMARY KEY (member_id),
         FOREIGN KEY (social_handles) REFERENCES social_handles(social_handles_id)   
     );";
@@ -36,16 +36,16 @@
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE past_members (
-        `name` varchar(255),
-        `roll_no` varchar(255),
-        `image` longblob,
-        `description`varchar(12000),
-        `member_id` varchar(255),
-        `hof` varchar(255),
-        `social_handles` varchar(255),
-        `post` varchar(255),
-        `wing` varchar(255),
-        `session` varchar(255),
+        name varchar(255),
+        roll_no varchar(255),
+        image longblob,
+        descriptionvarchar(12000),
+        member_id varchar(255),
+        hof varchar(255),
+        social_handles varchar(255),
+        post varchar(255),
+        wing varchar(255),
+        session varchar(255),
         PRIMARY KEY (member_id),
         FOREIGN KEY (social_handles) REFERENCES social_handles(social_handles_id)   
     );";
@@ -53,11 +53,11 @@
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE credentials (
-        `credentialsID` varchar(255),
-        `username` varchar(255),
-        `password` varchar(255),
-        `admin_value`varchar(255),
-        `member_id` varchar(255),
+        credentialsID varchar(255),
+        username varchar(255),
+        password varchar(255),
+        admin_valuevarchar(255),
+        member_id varchar(255),
         PRIMARY KEY (credentialsID),
         FOREIGN KEY (member_id) REFERENCES member(member_id)   
     );";
@@ -65,25 +65,25 @@
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE wings (
-        `wing_id` varchar(255),
-        `wing` varchar(255),
-        `info` varchar(12000),
-        `logo` longblob,
-        `image` longblob,                
+        wing_id varchar(255),
+        wing varchar(255),
+        info varchar(12000),
+        logo longblob,
+        image longblob,                
         PRIMARY KEY (wing_id)
     );";
 
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE Projects (
-        `wing_id` varchar(255),
-        `member_id` varchar(255),
-        `project_name` varchar(255),
-        `project_link`varchar(255),
-        `blog_link`varchar(255),
-        `source_code_link`varchar(255),
-        `description`varchar(12000),
-        `image` longblob,        
+        wing_id varchar(255),
+        member_id varchar(255),
+        project_name varchar(255),
+        project_linkvarchar(255),
+        blog_linkvarchar(255),
+        source_code_linkvarchar(255),
+        descriptionvarchar(12000),
+        image longblob,        
         FOREIGN KEY (wing_id) REFERENCES wings(wing_id),
         FOREIGN KEY (member_id) REFERENCES member(member_id)
     );";
@@ -91,12 +91,12 @@
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE blogs (
-        `wing_id` varchar(255),
-        `member_id` varchar(255),
-        `blog_title` varchar(255),
-        `description`varchar(12000),
-        `blog_link`varchar(255),        
-        `image` longblob,        
+        wing_id varchar(255),
+        member_id varchar(255),
+        blog_title varchar(255),
+        descriptionvarchar(12000),
+        blog_linkvarchar(255),        
+        image longblob,        
         FOREIGN KEY (member_id) REFERENCES member(member_id),
         FOREIGN KEY (wing_id) REFERENCES wings(wing_id)        
     );";
@@ -104,30 +104,30 @@
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE announcements (
-        `member_id` varchar(255),
-        `name` varchar(255),
-        `organizer`varchar(255),
-        `venue`varchar(255),        
-        `date`varchar(255),        
-        `time`varchar(255),        
-        `topic`varchar(255),        
-        `details`varchar(12000),        
-        `link`varchar(255),        
-        `attachment`varchar(255),        
-        `image` longblob,        
+        member_id varchar(255),
+        name varchar(255),
+        organizervarchar(255),
+        venuevarchar(255),        
+        datevarchar(255),        
+        timevarchar(255),        
+        topicvarchar(255),        
+        detailsvarchar(12000),        
+        linkvarchar(255),        
+        attachmentvarchar(255),        
+        image longblob,        
         FOREIGN KEY (member_id) REFERENCES member(member_id)
     );";
 
     pg_query($connection,$query) ;
 
     $query = "CREATE TABLE contact (
-        `made_on` varchar(255),
-        `first_name` varchar(255),
-        `last_name`varchar(255),
-        `description`varchar(12000),        
-        `email`varchar(255),        
-        `mobile`varchar(255),        
-        `gender`varchar(255)               
+        made_on varchar(255),
+        first_name varchar(255),
+        last_namevarchar(255),
+        descriptionvarchar(12000),        
+        emailvarchar(255),        
+        mobilevarchar(255),        
+        gendervarchar(255)               
     );";
 
     pg_query($connection,$query) ;
